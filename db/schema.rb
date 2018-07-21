0f3c916eda2f6a97d4f99f3c81722dee2442aa51
DanieleFedeli <danieligno10@gmail.com>
2018-07-21 21:01:50 +0200

    Added admin column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_20_185840) do
+ActiveRecord::Schema.define(version: 2018_07_21_190020) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 2018_07_20_185840) do
     t.string "uid"
     t.string "name"
     t.text "image"
+    t.string "boolean", default: "f"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
