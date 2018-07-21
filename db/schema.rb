59e7765dcd729197681c75bc3e4260ff676df134
DanieleFedeli <danieligno10@gmail.com>
2018-07-21 21:16:44 +0200

    .

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_21_190020) do
+ActiveRecord::Schema.define(version: 2018_07_21_191603) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 2018_07_21_190020) do
     t.string "name"
     t.text "image"
     t.string "boolean", default: "f"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
