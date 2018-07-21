5136e0f750520f56a4e409d6e177a4eed0e1f132
DanieleFedeli <danieligno10@gmail.com>
2018-07-21 16:05:24 +0200

    Redirect url fix

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -257,7 +257,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, "408791222974805", "7b283596f50b21ec75bf655ceafb1354", callback_url: "http://localhost:3000/users/auth/facebook/callback"
+
+  config.omniauth :facebook, "408791222974805", "7b283596f50b21ec75bf655ceafb1354", callback_url: "https://blogify123.herokuapp.com/users/auth/facebook/callback"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
