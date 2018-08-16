c06101ba08ba6a75cf37e01adaf98bbd76297089
DanieleFedeli <danieligno10@gmail.com>
2018-08-16 03:33:20 +0200

    :

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.all.each do |u|
-  u.destroy!
+#User.all.each do |u|
+#  u.destroy!
+#end
+
+#100.times do 
+#  User.create!(username: Faker::Internet.username, email: Faker::Internet.safe_email, password: 'ciaociao123', password_confirmation: 'ciaociao123')
+#end
+
+User.all.each do
+  10.times do
+    Post.create do |post|
+      post.title = Faker::WorldCup.team
+      post.body = Faker::Lorem.paragraphs(3)
+      list = []
+      4.times do 
+        list << Faker::Company.name
+      end
+      post.tag_list = list
+    end
+  end
 end
